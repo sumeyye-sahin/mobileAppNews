d08deef3672f22cf2718fa40ae84c5aee687d387
Sümeyye Şahin <smyysahin823@gmail.com>
2023-06-08 02:54:03 +0300

    update

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -2,19 +2,17 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './NewsCard.Style';
 
-const NewsCard = ({news}) =>{
-    
-    return(
-        <View style={styles.container}>
-            <Image style={styles.image} source={{ uri:news.imageUrl}}/>
-            <View style={styles.inner_container}>
-              <Text style={styles.title}> {news.title} </Text>  
-              <Text style={styles.description}> {news.description} </Text>
-              <Text style={styles.author} > {news.author} </Text>
-            </View>
-         
-        </View>
-    );
-}
+const NewsCard = ({news}) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{uri: news.imageUrl}} />
+      <View style={styles.inner_container}>
+        <Text style={styles.title}> {news.title} </Text>
+        <Text style={styles.description}> {news.description} </Text>
+        <Text style={styles.author}> {news.author} </Text>
+      </View>
+    </View>
+  );
+};
 
 export default NewsCard;
